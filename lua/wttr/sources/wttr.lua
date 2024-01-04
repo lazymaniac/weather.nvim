d7b0e0e0dd967a74caecf45c4a078a8353186808
Sebastian Fabisz <seba@Asus.>
2024-01-04 23:20:42 +0000

    debu

--- lua/wttr/sources/wttr.lua
+++ lua/wttr/sources/wttr.lua
@@ -8,6 +8,7 @@ result.get_raw = function(location, format, custom_format, callback)
 	if not util.is_empty(custom_format) then
 		format = custom_format
 	end
+	print("format", format)
 	curl.get({
 		url = "wttr.in/" .. location .. "?format=" .. format,
 		callback = function(response)
@@ -21,7 +22,6 @@ end
 -- Gets a response from wttr
 result.get = function(location, format, custom_format, callback)
 	result.get_raw(location, format, custom_format, function(response)
-		print(response.body)
 		callback(response.body)
 	end)
 end
@@ -31,6 +31,7 @@ result.get_forecast = function(location, callback)
 		url = "wttr.in",
 		callback = function(response)
 			vim.schedule(function()
+				print(response.body)
 				callback(response.body)
 			end)
 		end,
