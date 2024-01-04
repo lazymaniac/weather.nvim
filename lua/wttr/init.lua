3ae066081612cb2e05b06aa52a95c7f464d7df79
Sebastian Fabisz <seba@Asus.>
2024-01-04 22:53:07 +0000

    debug

--- lua/wttr/init.lua
+++ lua/wttr/init.lua
@@ -5,13 +5,11 @@ local config = require("wttr.default_config").default
 local util = require("wttr.util")
 
 local weather = {}
-local inc = 0
 local timer = nil
 
 local function get_weather()
 	local result = wttr.get(function(data)
-		inc = inc + 1
-		weather.text = data .. inc
+		weather.text = data
 
 		vim.schedule(function()
 			vim.api.nvim_command("redrawstatus")
