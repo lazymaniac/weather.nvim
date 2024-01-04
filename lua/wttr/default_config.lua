c52cbd7d500068abd515b534f5c51fa5eeff8049
Sebastian Fabisz <seba@Asus.>
2024-01-04 23:28:33 +0000

    debug

--- lua/wttr/default_config.lua
+++ lua/wttr/default_config.lua
@@ -4,7 +4,7 @@ result.default = {
 	location = "", -- Optional location. If empty wttr.in will determine location by IP
 	format = 1, -- Type of format. Values from 1 to 4
 	custom_format = "", -- Custom format. If empty then format field is used
-	update_interval = 4000, -- 15 Minutes in ms
+	update_interval = 15 * 60 * 1000, -- 15 Minutes in ms
 }
 
 return result
