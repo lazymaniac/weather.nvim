4b2261fc923edd3f0d1893034f17ac54168df23a
Sebastian Fabisz <seba@Asus.>
2024-01-05 05:26:55 +0000

    remove debug

--- lua/wttr/init.lua
+++ lua/wttr/init.lua
@@ -14,7 +14,6 @@ wttr.text = "Pending"
 local function get_weather(format, custom_format)
 	local result = wttr_src.get(location, format, custom_format, function(data)
 		wttr.text = string.gsub(data, "%%", "")
-		print("text: ", wttr.text)
 		vim.schedule(function()
 			vim.api.nvim_command("redrawstatus")
 		end)
