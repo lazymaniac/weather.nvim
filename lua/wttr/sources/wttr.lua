f2923e153d894ebc58d67e09153ee575fa579558
Sebastian Fabisz <seba@Asus.>
2024-01-05 00:00:36 +0000

    change url

--- lua/wttr/sources/wttr.lua
+++ lua/wttr/sources/wttr.lua
@@ -31,7 +31,7 @@ end
 
 result.get_forecast = function(location, callback)
 	curl.get({
-		url = "v3.wttr.in/Bayern.sxl",
+		url = "v3d.wttr.in/" .. location,
 		callback = function(response)
 			vim.schedule(function()
 				callback(response.body)
